d0425e00ca8a585645d14e1648581f9d2fa257b7
prajwalrajbr <42075029+prajwalrajbr@users.noreply.github.com>
2020-03-25 18:14:01 +0530

    Added a function to check if the value is repeated in the corresponding row or column or in its 3X3 box

--- main.py
+++ main.py
@@ -1,17 +1,77 @@
 
 board=[
-    [1,2,0,4,5,6,7,8,9],
-    [1,2,3,4,5,0,7,8,9],
-    [1,2,0,4,5,6,7,8,9],
-    [9,2,3,0,5,6,7,8,9],
-    [1,2,3,4,5,0,7,8,9],
-    [1,2,3,0,5,6,7,8,9],
-    [6,2,3,4,5,6,7,8,9],
-    [1,2,3,0,5,6,7,8,9],
-    [1,2,3,4,5,0,7,8,9]
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,5],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
       ]
 
+def checker(value,row,col):
+    global board
+    #Row checker
+    for i in range(0,9):
+        if board[i][col]==value and i!=row:
+            return False
 
+    #Column checker
+    for i in range(0,9):
+        if board[row][i]==value and i!=col:
+            return False
+
+    #Corresponding 3X3 box checker
+    if row%3==0:
+        if col%3==0:
+            for i in [row,row+1,row+2]:
+                for j in [col,col+1,col+2]:
+                    if board[i][j]==value:
+                        return False
+        elif col%3==1:
+            for i in [row,row+1,row+2]:
+                for j in [col-1,col,col+1]:
+                    if board[i][j]==value:
+                        return False
+        else:
+            for i in [row,row+1,row+2]:
+                for j in [col-2,col-1,col]:
+                    if board[i][j]==value:
+                        return False
+    elif row%3==1:
+        if col%3==0:
+            for i in [row-1,row,row+1]:
+                for j in [col,col+1,col+2]:
+                    if board[i][j]==value:
+                        return False       
+        elif col%3==1:
+            for i in [row-1,row,row+1]:
+                for j in [col-1,col,col+1]:
+                    if board[i][j]==value:
+                        return False
+        else:
+            for i in [row-1,row,row+1]:
+                for j in [col-2,col-1,col]:
+                    if board[i][j]==value:
+                        return False
+    else:
+        if col%3==0:
+            for i in [row-2,row-1,row]:
+                for j in [col,col+1,col+2]:
+                    if board[i][j]==value:
+                        return False       
+        elif col%3==1:
+            for i in [row-2,row-1,row]:
+                for j in [col-1,col,col+1]:
+                    if board[i][j]==value:
+                        return False
+        else:
+            for i in [row-2,row-1,row]:
+                for j in [col-2,col-1,col]:
+                    if board[i][j]==value:
+                        return False
 
 def showBoard():
     global board
@@ -30,7 +90,4 @@ def showBoard():
         if c1%3==0:
             print("-------------------------")       
         
-        
-
-
 showBoard()
